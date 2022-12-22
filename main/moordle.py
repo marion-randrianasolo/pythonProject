bce1bb77d6391b5c9a51aedf897150090ca6f9f9
Marion Randrianasolo <marion.randrianasolo@etu.umontpellier.fr>
2022-12-22 02:21:04 +0100

    game

---
+++ main/moordle.py
@@ -0,0 +1,39 @@
+from main.quiz import questions
+
+
+class Moordle:
+
+    # Maximum attempts for a question
+    MAX_ATTEMPTS = 6
+
+    def __init__(self, secret: str):
+        self.secret: str = secret.upper()
+        self.attempts = []
+        pass
+
+    # Attemps to guess
+    def attempt(self, word: str):
+        self.attempts.append(word)
+
+    # Load data from a py file
+    def load_question_set(path: str):
+        with open(path, "r"):
+            for key in questions:
+                key, questions.get(key)
+        return questions
+
+    # Returns true if the question is solved
+    @property
+    def is_solved(self):
+        return len(self.attempts) > 0 and self.attempts[-1] == self.secret
+
+    # Returns remaining attempts
+    @property
+    def remaining_attempts(self) -> int:
+        return self.MAX_ATTEMPTS - len(self.attempts)
+
+    # Returns true if able to attempt
+    @property
+    def can_attempt(self):
+        return self.remaining_attempts > 0 and not self.is_solved
+        pass
